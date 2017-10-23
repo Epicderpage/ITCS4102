b591a790cbb1f8e8d8e45dd8749fff1e0f674354
Haely Pratt <haely.pratt@gmail.com>
2017-10-23 12:59:44 -0400

    minor edits to internal documentation

--- Part2/2-BalancedParentheses/balanced.pl
+++ Part2/2-BalancedParentheses/balanced.pl
@@ -3,20 +3,20 @@
 use strict;
 use warnings;
 
-#Take in input
+# Take in input
 $_ = $ARGV[0] or die "Not enough arguments. Expected string of parenthetic characters.\n";
 
-#Loop and remove each matched pair
+# Loop and remove each matched pair
 while (1){
-	#If we can match a pair, remove it
+	# If we can match a pair, remove it
 	if (s/(\{\}|\[\]|\(\))//xs){
-		#If the string is empty its balanced
+		# If the string is empty, it's balanced
 		if ($_ eq ''){
 			print "Balanced!\n";
 			last;
 		}
 	} else {
-		#If we can't match then the string is unbalanced
+		# If we can't match, then the string is unbalanced
 		print "Unbalanced\n";
 		last;
 	}
