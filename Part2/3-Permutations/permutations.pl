33638517d217a2ffd7c99a50de42422194da03dc
Haely Pratt <haely.pratt@gmail.com>
2017-10-23 13:02:28 -0400

    minor edits for code consistency

--- Part2/3-Permutations/permutations.pl
+++ Part2/3-Permutations/permutations.pl
@@ -2,14 +2,14 @@
 
 use strict;
 use warnings;
-use Data::Dumper qw(Dumper);
+#use Data::Dumper qw(Dumper);
 
-#Process input
+# Process input
 our $input = $ARGV[0] or die "Not enough arguments. Expected string of characters.\n";
 my @chars = split(//, $input);
 my $len = scalar @chars;
 
-#Simple print function for the array 
+# Simple print function for the string 
 sub printA {
 	foreach my $p (@_){
 		print $p;
@@ -17,9 +17,8 @@ sub printA {
 	print "\n";
 }
 
-
-#Heap's algorithm (B. R. Heap, 1963)
-my $i=0;
+# Heap's algorithm (B. R. Heap, 1963)
+my $i = 0;
 my @j = (0) x $len;
 printA @chars;
 
@@ -28,11 +27,11 @@ while ($i < $len){
 		if (($i % 2) == 0) {
 			@chars[0,$i] = @chars[$i,0];
 		} else {
-			@chars[$j[$i],$i] = @chars[$i, $j[$i]];
+			@chars[$j[$i],$i] = @chars[$i,$j[$i]];
 		}
-		printA @chars;
+    printA @chars;
 		$j[$i]++;
-		$i=0;
+		$i = 0;
 	} else {
 		$j[$i] = 0;
 		$i++;
